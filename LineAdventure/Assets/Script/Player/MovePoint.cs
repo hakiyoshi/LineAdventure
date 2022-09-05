b0f00f2648465c3633cb767e8c3d5eb02ddf321b
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-09-06 05:55:13 +0900

    壁検知の不具合修正
    最短経路での移動のみしか考慮していなかったため最長経路で移動できるか確認して作成

--- LineAdventure/Assets/Script/Player/MovePoint.cs
+++ LineAdventure/Assets/Script/Player/MovePoint.cs
@@ -68,7 +68,7 @@ public class MovePoint : MonoBehaviour
                 moveVec.normalized, (point.transform.position - a.position).normalized);
             if (dot > nearDot && CheckMove(nearbyPoints, point))
             {
-                CheckMove(nearbyPoints, point);
+                //CheckMove(nearbyPoints, point);
                 nearPoint = point.transform;
                 nearDot = dot;
             }
@@ -78,7 +78,7 @@ public class MovePoint : MonoBehaviour
                    point.transform.position == nearPoint.position && 
                    point.transform.rotation == a.rotation && CheckMove(nearbyPoints, point))
                 {
-                    CheckMove(nearbyPoints, point);
+                    //CheckMove(nearbyPoints, point);
                     nearPoint = point.transform;
                     nearDot = dot;
                 }
@@ -92,24 +92,41 @@ public class MovePoint : MonoBehaviour
     private bool CheckMove(NearbyPoints basePoint, NearbyPoints movePoint)
     {
         var basePosi = basePoint.transform.position;
-        
+        var moveVec = movePoint.transform.position - basePosi;
+        var playerVec = lineCurve.StandardControlPosition - basePosi;
+
         foreach (var wallPoint in basePoint.WallPoints)
         {
             //ベクトル
-            var moveVec = movePoint.transform.position - basePosi;
-            var playerVec = lineCurve.StandardControlPosition - basePosi;
             var wallVec = wallPoint.transform.position - basePosi;
-            
+
             //内積
             var playerCross = Vector3.Cross(wallVec.normalized, playerVec.normalized);
             var moveCross = Vector3.Cross(wallVec.normalized, moveVec.normalized);
-            
+
             //お互いの富豪が違う場合壁を挟んでいる
-            if(Mathf.Approximately(playerCross.z, 0.0f) || Mathf.Approximately(moveCross.z, 0.0f))
+            if(Mathf.Approximately(moveCross.z, 0.0f) || Mathf.Approximately(playerCross.z, 0.0f))
                 continue;
-            
-            if (!Mathf.Approximately(Math.Sign(playerCross.z), Math.Sign(moveCross.z)))
-                return false;
+
+            var playerSign = Mathf.Sign(playerCross.z);
+            if (!Mathf.Approximately(playerSign, Mathf.Sign(moveCross.z)))
+            {
+                //最短経路でヒットした場合最長経路でヒットするかチェックする
+                foreach (var wall in basePoint.WallPoints)
+                {
+                    if (wall == wallPoint || wall == movePoint)
+                        continue;
+                    
+                    wallVec = wall.transform.position - basePosi;
+                    playerCross = Vector3.Cross(wallVec.normalized, playerVec.normalized);
+                    
+                    if(Mathf.Approximately(playerCross.z, 0.0f))
+                        continue;
+
+                    if (Mathf.Approximately(-playerSign, Mathf.Sign(playerCross.z)))
+                        return false;
+                }
+            }
         }
 
         return true;
